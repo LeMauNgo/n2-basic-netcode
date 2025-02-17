511224acc2ec35b73a8244f1d5ee058d87fb16d4
Simon Sai <tranminhduc18116@gmail.com>
2025-02-17 22:54:38 +0700

    finish PlayerServer

--- Assets/_Data/Player/PlayerController.cs
+++ Assets/_Data/Player/PlayerController.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 public class PlayerController : NetworkBehaviour
 {
     public float moveSpeed = 5f;
+    private Vector3 moveInput;
 
     void Update()
     {
         if (!IsOwner) return;
 
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        moveInput = new Vector3(moveX, 0, moveZ);
 
-        transform.Translate(new Vector3(moveX, 0, moveZ));
+        SendMovementToServerServerRpc(moveInput);
+    }
+
+    [ServerRpc]
+    private void SendMovementToServerServerRpc(Vector3 moveDirection, ServerRpcParams rpcParams = default)
+    {
+        if (!IsServer) return;
+
+        if (NetworkManager.Singleton.ConnectedClients.TryGetValue(rpcParams.Receive.SenderClientId, out var client))
+        {
+            if (client.PlayerObject.TryGetComponent(out PlayerController player))
+            {
+                player.transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            }
+        }
     }
 }
