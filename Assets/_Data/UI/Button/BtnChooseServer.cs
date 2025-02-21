a5a28b88431d7296ea1d185762f0e42c7cea5ed9
Simon Sai <tranminhduc18116@gmail.com>
2025-02-21 16:18:43 +0700

    remove unuse code

--- Assets/_Data/UI/Button/BtnChooseServer.cs
+++ Assets/_Data/UI/Button/BtnChooseServer.cs
@@ -16,49 +16,13 @@ public class BtnChooseServer : ButttonAbstract
     {
         base.Start();
         this.serverInfo = ServerManager.Instance.GetServer(this.serverInfo.ServerName);
-
         this.UpdateUI();
-        //TODO: this is not working yet
-        //InvokeRepeating(nameof(this.CheckServerStatus), 1, 3f);
     }
 
     public override void OnClick()
     {
         Debug.Log("Choose server: " + this.serverInfo.ServerName);
         ServerManager.Instance.ConnectToServer(this.serverInfo.ServerName);
-    }
-
-    public void CheckServerStatus()
-    {
-        Thread thread = new Thread(() =>
-        {
-            bool isOnline = IsServerOnline(this.serverInfo.IPAddress, this.serverInfo.Port);
-            Debug.Log(isOnline ? "Server is ONLINE" : "Server is OFFLINE");
-
-        });
-        thread.Start();
-    }
-
-    private bool IsServerOnline(string ip, int port)
-    {
-        try
-        {
-            using (TcpClient client = new TcpClient())
-            {
-                client.Connect(ip, port);
-                return true;  // Server is running
-            }
-        }
-        catch
-        {
-            return false;  // Server is not running
-        }
-    }
-
-    private IEnumerator UpdateServerStatus(bool isOnline)
-    {
-        yield return null; 
-        Debug.Log(isOnline ? "Server is ONLINE" : "Server is OFFLINE");
     }
 
     private void UpdateUI()
